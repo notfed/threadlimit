f3f51749361e3951594cb47c2e30a998da5ae472
Jay Sullivan <jay@petio.org>
2010-09-17 16:03:39 -0400

    try.cpp : print A through Z in groups of 5

--- try.cpp
+++ try.cpp
@@ -9,15 +9,16 @@ void* thread_func(void* arg)
 {
   sleep(1);
   const char *ptr = (const char*)arg;
-  {
-    Lock<Mutex> guard(console_lock);
+
+  { Lock<Mutex> guard(console_lock);
     cout << "Thread " << ptr << endl;
   }
+
   return 0;
 }
 int main()
 {
-  ThreadLimit pool(2);
+  ThreadLimit pool(5);
   pool.Add(thread_func,(void*)"A"); 
   pool.Add(thread_func,(void*)"B");
   pool.Add(thread_func,(void*)"C");
@@ -28,6 +29,22 @@ int main()
   pool.Add(thread_func,(void*)"H");
   pool.Add(thread_func,(void*)"I");
   pool.Add(thread_func,(void*)"J");
+  pool.Add(thread_func,(void*)"K");
+  pool.Add(thread_func,(void*)"L");
+  pool.Add(thread_func,(void*)"M");
+  pool.Add(thread_func,(void*)"N");
+  pool.Add(thread_func,(void*)"O");
+  pool.Add(thread_func,(void*)"P");
+  pool.Add(thread_func,(void*)"Q");
+  pool.Add(thread_func,(void*)"R");
+  pool.Add(thread_func,(void*)"S");
+  pool.Add(thread_func,(void*)"T");
+  pool.Add(thread_func,(void*)"U");
+  pool.Add(thread_func,(void*)"V");
+  pool.Add(thread_func,(void*)"W");
+  pool.Add(thread_func,(void*)"X");
+  pool.Add(thread_func,(void*)"Y");
+  pool.Add(thread_func,(void*)"Z");
   pool.Finish();
   return 0;
 }
