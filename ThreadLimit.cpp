f213a294901a540871d30b86013a32ef7a714708
Jay Sullivan <jay@petio.org>
2010-09-17 12:26:14 -0400

    test for q.empty() instead of q.size()>0

--- ThreadLimit.cpp
+++ ThreadLimit.cpp
@@ -2,7 +2,9 @@
 #include "ThreadLimit.h"
 #include "CallMemFun.h"
 #include "Lock.h"
+#include <iostream>
 #include <queue>
+using namespace std;
 
 //
 // ThreadStarter
@@ -38,6 +40,7 @@ ThreadLimit::ThreadLimit(int limit)
     m_Available(limit),
     m_Thread(&CallMemFun<ThreadLimit,&ThreadLimit::ThreadLimitLoop>,this)
 {
+  m_Thread.Start();
 }
 ThreadLimit::~ThreadLimit()
 {
@@ -45,47 +48,51 @@ ThreadLimit::~ThreadLimit()
 }
 void* ThreadLimit::ThreadLimitLoop()
 {
-  int queued;
+  bool empty;
   int available;
   for(;;)
   {
     // Safely fetch queue size
     { Lock<Mutex> guard(m_QueueLock);
-      queued = m_ThreadQueue.size(); }
+      empty = m_ThreadQueue.empty(); }
 
     // Wait for a thread to be queued
-    if(queued==0) 
+    if(empty)  {
+      cerr << "ThreadLimit: No threads are queued. Waiting for a thread to be queued." << endl;
       m_AddEvent.Wait(); 
+    }
     
     // Safely fetch m_Available
     { Lock<Mutex> guard(m_QueueLock);
       available = m_Available; }
 
     // Wait for an available thread slot
-    if(available==0) 
+    if(available==0)  {
+      cerr << "ThreadLimit: Limit reached.  Waiting for a thread to finish." << endl;
       m_FinishEvent.Wait(); 
+    }
 
     // Run the next thread
     {
+      cerr << "ThreadLimit: Executing next thread" << endl;
       Lock<Mutex> guard(m_QueueLock);
-      if(m_ThreadQueue.empty()) continue;
-      --m_Available;
       ThreadStarter nextThread = m_ThreadQueue.front();
       m_ThreadQueue.pop();
-      nextThread.Start();
+      --m_Available; nextThread.Start();
     }
   }
   return 0;
 }
 void ThreadLimit::SignalFinish()
 {
+      cerr << "ThreadLimit: A thread finished." << endl;
       Lock<Mutex> guard(m_QueueLock);
-      ++m_Available;
-      m_FinishEvent.Signal();
+      ++m_Available; m_FinishEvent.Signal();
 }
 void ThreadLimit::Add(Start_routine_f start_routine, Start_routine_arg_t start_routine_arg)
 {
     Lock<Mutex> guard(m_QueueLock);
     m_ThreadQueue.push(ThreadStarter(start_routine,start_routine_arg, this));
+    cerr << "ThreadLimit: Added a new thread." << endl;
     m_AddEvent.Signal();
 }
